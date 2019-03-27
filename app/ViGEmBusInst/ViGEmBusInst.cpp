8eb2381e3346ded81a4498f2fd8adcdcb36a413a
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2019-03-27 19:37:10 +0100

    Added dependencies Set linker manifest option for UAC elevation

--- app/ViGEmBusInst/ViGEmBusInst.cpp
+++ app/ViGEmBusInst/ViGEmBusInst.cpp
@@ -1,11 +1,11 @@
 // ViGEmBusInst.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include "ViGEmBusInst.h"
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
---
+++ app/ViGEmBusInst/ViGEmBusInst.h
@@ -0,0 +1,32 @@
+#pragma once
+
+//
+// Windows
+// 
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <SetupAPI.h>
+#include <newdev.h>
+
+//
+// Device class interfaces
+// 
+#include <initguid.h>
+#include <devguid.h>
+
+//
+// STL
+// 
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+//
+// CLI argument parser
+// 
+#include "argh.h"
+
+#include "colorwin.hpp"
+
+#include "Devcon.h"
